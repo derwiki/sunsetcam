84ce01374a23a702234bbd62fd0377aa98297dc4
Adam Derewecki <derewecki@gmail.com>
2017-11-09 10:24:07 -0800

    /upload stores to s3, / retrieves latest image

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,6 +1,25 @@
 class VisitorsController < ApplicationController
-  skip_before_action :verify_authenticity_token
-  def upload
-    render plain: "#{params[:image].tempfile.length} bytes OK"
+  skip_before_action :verify_authenticity_token, only: :upload
+
+  def index
+    @photo = Photo.order('id desc').first
   end
+
+  #TODO basic http auth?
+  def upload
+    key = "#{Time.now.to_s(:db).gsub(/[ :]/, ?-)}.jpg"
+    obj = upload_to_s3(key)
+    Photo.create!(path: key)
+
+    render plain: obj.public_url
+  end
+
+  private
+
+    def upload_to_s3(key)
+      s3 = Aws::S3::Resource.new
+      s3.bucket('potrerohillcam').object(key).tap do |obj|
+        obj.upload_file(params[:image].tempfile, acl: 'public-read')
+      end
+    end
 end
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,2 +1,5 @@
 class Photo < ApplicationRecord
+  def url
+    Aws::S3::Resource.new.bucket('potrerohillcam').object(self.path).public_url
+  end
 end
