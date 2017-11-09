78c7468e05f671df98b6c321343f0a045acbe8ce
Adam Derewecki <derewecki@gmail.com>
2017-11-09 09:41:51 -0800

    /upload

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,2 +1,6 @@
 class VisitorsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+  def upload
+    render plain: "#{params[:image].tempfile.length} bytes OK"
+  end
 end
